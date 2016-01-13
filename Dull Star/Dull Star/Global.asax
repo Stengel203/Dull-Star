d6c2928116e9e2e2f95e526a2cfc449839312b79
Stengel <tom_dhulster@hotmail.com>
2016-01-13 14:11:50 +0100

    Initial project commit + connection to database

---
+++ Dull Star/Dull Star/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dull_Star.MvcApplication" Language="C#" %>
---
+++ Dull Star/DullStarData/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DullStarData.DAL;
+
+namespace DullStarData.Repositories
+{
+    public class UserRepository
+    {
+        private DullStarDevEntities _context;
+
+        public UserRepository()
+        {
+            _context = new DullStarDevEntities();
+        }
+
+        public UserRepository(DullStarDevEntities context)
+        {
+            _context = context;
+        }
+
+        public void createUser(string Id, string UserName, DateTime BirthDate)
+        {
+            Users user = new Users
+            {
+                ANU_Id = Id,
+                UserName = UserName,
+                BirthDate = BirthDate
+            };
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+    }
+}
